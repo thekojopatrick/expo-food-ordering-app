9b718e897e5a41673392fa25880f7a073fb4edcd
Kojo Patrick <patrickasare84@gmail.com>
2024-04-15 12:30:14 +0000

    Remove modal screen and move its link to cart screen

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -4,17 +4,10 @@ import { Text, View } from '@components/Themed';
 import EditScreenInfo from '@components/EditScreenInfo';
 import { StatusBar } from 'expo-status-bar';
 
-export default function ModalScreen() {
+export default function CartScreen() {
 	return (
 		<View style={styles.container}>
-			<Text style={styles.title}>Modal</Text>
-			<View
-				style={styles.separator}
-				lightColor='#eee'
-				darkColor='rgba(255,255,255,0.1)'
-			/>
-			<EditScreenInfo path='app/modal.tsx' />
-
+			<Text style={styles.title}>Cart</Text>
 			{/* Use a light status bar on iOS to account for the black space above the modal */}
 			<StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
 		</View>
